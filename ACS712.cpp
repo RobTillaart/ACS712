0474549aad6e36e8bd03f145ce8169d2a7f0895c
rob tillaart <rob.tillaart@gmail.com>
2020-03-21 21:48:45 +0100

    0.1.2 version

---
+++ ACS712.cpp
@@ -0,0 +1,73 @@
+//
+//    FILE: ACS712.cpp
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.1.2
+//    DATE: 2020-03-17
+// PURPOSE: ACS712 library - current measurement
+//
+// HISTORY:
+// 0.1.0    2020-03-17 initial version
+// 0.1.1    2020-03-18 first release version
+// 0.1.2	2020-03-21 automatic formfactor test
+//
+// Released to the public domain
+//
+
+#include "ACS712.h"
+
+ACS712::ACS712(uint8_t analogPin, float volts, uint16_t maxADC, uint8_t mVperA)
+{
+    _pin = analogPin;
+    // 1x 1000 for V -> mV
+    _mVpstep = 1000.0 * volts / maxADC;
+    _mVperAmpere = mVperA;
+    _formFactor = 0.70710678119;  // 0.5 * sqrt(2);  TODO: should be smaller in practice 0.5 ?
+    _midPoint = maxADC / 2;
+}
+
+int ACS712::mA_AC(uint8_t freq)
+{
+    uint32_t start = micros();
+    uint16_t period = ((freq == 60) ? 16670 : 20000);
+    uint8_t samples = 0;
+    uint8_t zeros = 0;
+    int _min, _max;
+    _min = _max = analogRead(_pin);
+    while (micros() - start < period)  // UNO ~180 samples...
+    {
+        samples++;
+        int val = analogRead(_pin);
+        if (val < _min) _min = val;
+        if (val > _max) _max = val;
+        if (abs(val - _midPoint) < 4) zeros++;   // Noise max 4 during test
+    }
+    int p2p = (_max - _min);
+
+    // automatic determine _formFactor / crest factor   
+    float D = 0;	
+    float FF = 0;
+    if (zeros > 5)                          // MAGIC NUMBER 5 ~2.5% of samples = zero
+    {
+      D = 1.0 - (1.0 * zeros) / samples;    // % SAMPLES NONE ZERO
+      FF = sqrt(D) * 0.5 * sqrt(2);         // ASSUME NON ZERO PART ~ SINUS
+	}
+	else  // # zeros is small => D --> 1 --> sqrt(D) --> 1
+	{
+      FF = 0.5 * sqrt(2);
+    }
+    _formFactor = FF;
+
+    // math could be partially precalculated: C = 1000.0 * 0.5 * _mVpstep / _mVperAmpere;
+    // rounding?
+    return 1000.0 * 0.5 * p2p * _mVpstep * _formFactor / _mVperAmpere;
+}
+
+int ACS712::mA_DC()
+{
+    // read twice to stabilize...
+    analogRead(_pin);
+    int steps = analogRead(_pin) - _midPoint;
+    return 1000.0 * steps * _mVpstep / _mVperAmpere;
+}
+
+// END OF FILE
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # ACS712
-ACS Current Sensor - 5A, 20A, 30A
+
+Current Sensor - 5A, 20A, 30A
+
+## Description
+
+The ACS712 is a chip to measure current, both AC or DC. The chip has an 
+analog output that provides a voltage that is lineair with the current. 
+The ACS712 library supports only a built in ADC by means of analogRead().
+There are 2 core functions:
+
+* **int mA_DC()**
+* **int mA_AC()**
+
+To measure DC current a single analogRead() with some conversion math is sufficient to get 
+a value. To stabilize the signal analogRead() is called twice.
+
+To measure AC current **a blocking loop for 20 millis** is run to determine the 
+peak to peak value which is converted to the RMS value. To convert the peak2peak 
+value to RMS one need the so called crest or form factor. This factor depends heavily 
+on the signal form. For a perfect sinus the value is sqrt(2)/2.
+
+## Test
+
+The library is tested with the RobotDyn ACS712 20A breakout and an Arduino UNO.
+
+## Operation
+
+With the constructor the parameters **volts** and **maxADC (steps)** of the ADC are set 
+together with the **milliVolt per Ampere** value. The last parameter can be adjusted 
+afterwards, e.g. to callibrate this value runtime. Note this parameter affects both 
+AC and DC measurements.
+
+To callibrate the zero level for DC measurements, 4 functions are available to 
+adjust the midPoint.
+
+To callibrate the RMS value for AC measurements, 2 functions are available to 
+get and set the formFactor.
+
+The examples show the basic working of the functions.
+
